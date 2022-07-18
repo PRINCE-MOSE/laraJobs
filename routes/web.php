04bcc1807d2825cdead8a7c4600d85042dbb1b71
Moses S. Wekesa <MWekesa@helb.co.ke>
2022-07-18 17:02:57 +0300

    Initial commit Working

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Listings;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//All listings
+Route::get('/', function () {
+    return view('Listings',[
+        'heading'=>'Latest Jobs',
+        'listings'=>Listings::all()
+    ]);
+});
+
+//Single listing
+Route::get('/listings/{id}', function($id) {
+    return view('Listings',[
+        'listings'=>Listings::find($id)
+    ]);
+});
